3ccbc9553b438f053f2ab352de238bdd76d156b3
antron3000 <fortyonepointsix@gmail.com>
2017-10-14 21:28:27 -0400

    Update IBO.sol

--- contracts/IBO.sol
+++ contracts/IBO.sol
@@ -1,16 +1,18 @@
 import "./HumanStandardToken.sol";
 import "./Ownable.sol";
 
-pragma solidity 0.4.15;
+pragma solidity 0.4.17;
 
 contract IBO is Ownable{
+          using SafeMath for uint256;
+
 
  HumanStandardToken token;
 
     uint TokenValue; //Value of each token in wei
     address ProjectOwner;
 
-   uint8 decimals;
+    uint8 decimals;
 
     uint numBounties;
     uint numClaims;
@@ -36,6 +38,8 @@ contract IBO is Ownable{
         uint Available;   //bounties available
         bool CanBenefact;
         uint numClaims;
+        BountyType bountyType;
+        uint claimFraction; 
         mapping(uint=>Claim) claims;
        // BountyType bountyType;
        // uint claimFraction;
@@ -49,15 +53,17 @@ contract IBO is Ownable{
 
     }
 
-   // enum BountyType{Normal,Infinite}
+
+   // enum BountyType{Normal,Infinite,Milestone}
+      enum BountyType{Normal,Milestone,Infinite}
+
     enum ClaimState{Approved,Rejected,Pending}
 
 
     mapping(uint=>Bounty) Bounties;
-    mapping(uint => uint[]) claimsForBounty;
 
 
-    function CreateBounty(string _name,uint _reward,uint _available,bool _CanBenefact){
+    function CreateBountyNormal(string _name,uint _reward,uint _available,bool _CanBenefact){
 
 
         require(_available >= 1);
@@ -70,17 +76,48 @@ contract IBO is Ownable{
 
         numBounties++;
 
-        Bounties[numBounties] = Bounty(numBounties, _name, _reward, _available, _CanBenefact, 0);
+        Bounties[numBounties] = Bounty(numBounties, _name, _reward, _available, _CanBenefact, 0,BountyType.Normal,0);
 
     }
+    
+    function CreateBountyInfinite(string _name,uint _reward,uint _claimFraction){
+            require(_reward >= 1);
+            require((token.balanceOf(this) - markedTokenBalance)>= _reward);
+            markedTokenBalance += _reward;
+            numBounties++;
+            
+        Bounties[numBounties] = Bounty(numBounties,_name,_reward,1,false,0,BountyType.Infinite,_claimFraction); 
 
-    function CreateClaim(uint _BountyID, address _Claimer, bytes32 _SubmissionHash){
+    }
+      /*function CreateBountyMilestone(string _name,uint _reward){
+            require(_reward >= 1);
+            require((token.balanceOf(this) - markedTokenBalance)>= _reward);
+            markedTokenBalance += _reward;
+            numBounties++;
+            
+        Bounties[numBounties] = Bounty(numBounties,_name,_reward,1,false,0,BountyType.Infinite,_claimFraction); 
+
+    }*/
+
+    function CreateClaim(uint _BountyID, bytes32 _SubmissionHash){
       Bounty storage bounty =  Bounties[_BountyID];
       bounty.numClaims++;
+  if (bounty.bountyType == BountyType.Normal){
+      bounty.claims[bounty.numClaims] = Claim(_BountyID,bounty.numClaims,msg.sender,_SubmissionHash);
+      }else if(bounty.bountyType == BountyType.Infinite){
+          //calculate payout
+              uint payout = (bounty.Reward).mul(bounty.claimFraction).div(1000000);
+                          //transfer _tokenAddress
 
-      bounty.claims[bounty.numClaims] = Claim(_BountyID,bounty.numClaims,_Claimer,_SubmissionHash);
-    }
+            require(token.transfer(msg.sender, payout));
+                          //subtract from reward
 
+            bounty.Reward -= payout;
+
+              
+      }
+    
+}
   function Benefact(uint _BountyID) payable{
 
       Bounty storage _Bounty = Bounties[_BountyID];
@@ -103,8 +140,6 @@ contract IBO is Ownable{
 
       Bounties[_BountyID].Available -= numBounties;
       markedTokenBalance -= TotalReward;
-
-
   }
 
   function approveClaim(uint bountyId, uint claimID) {
